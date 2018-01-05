8cfbb8950be0f5bbe5dcc9d4c8333b6e4cc25608
PetrHalik <kimis.fin@gmail.com>
2018-01-05 13:20:31 +0100

    Refactor - code to modules

--- gopix.js
+++ gopix.js
@@ -5,11 +5,80 @@
 */
 var MapController = (function () {
 
+    // map constants
+    var MapConstants = {
+        DEFAULT_LATITUDE: 50.084218,
+        DEFAULT_LONGITUDE: 14.441196,
+        DEFAULT_ZOOM: 15,
+        DEFAULT_MAP_TYPE: 'terrain',
+    }
+
+    // variables
+
+    var map = null;
+    var currentLatitude = MapConstants.DEFAULT_LATITUDE;
+    var currentLongitude = MapConstants.DEFAULT_LONGITUDE;
+
+
+    /*
+        center map
+      */
+    var centerMap = function () {
+        map.setCenter(new google.maps.LatLng(parseFloat(currentLatitude), parseFloat(currentLongitude)));
+    };
+
+    /*
+    find position - cheated code sttructure
+    */
+    var findPosition = function () {
+        // get current location
+        // check if  Geolocation API is available
+        if (!navigator.geolocation) {
+            console.log('Geolocation API not available');
+            // Geolocation API not available - use default
+            currentLatitude = MapConstants.DEFAULT_LATITUDE;
+            currentLongitude = MapConstants.DEFAULT_LONGITUDE;
+
+        } else {
+            console.log('Geolocation API is available');
+            // asynchronous
+            navigator.geolocation.getCurrentPosition(function (position) {
+                    // Get the coordinates of the current possition.
+                    currentLatitude = position.coords.latitude;
+                    currentLongitude = position.coords.longitude;
+                    centerMap();
+                },
+                // Optional error callback
+                function (error) {
+                    //alert('Position is unknown. It will be used a default one! Error code: ' + error.code)
+                    console.log(error);
+                    currentLatitude = MapConstants.DEFAULT_LATITUDE;
+                    currentLongitude = MapConstants.DEFAULT_LONGITUDE;
+                    centerMap();
+                }
+            );
+        }
+    };
+
     /*
     --------------------- Return part ---------------------
     */
     return {
+        init: function (google) {
 
+            var mapElement, mapOptions;
+
+            // create map
+            mapOptions = {
+                zoom: MapConstants.DEFAULT_ZOOM,
+                mapTypeId: 'terrain'
+            };
+            mapElement = document.getElementById('map');
+            map = new google.maps.Map(mapElement, mapOptions);
+
+            // find position
+            findPosition();
+        },
     }
 
 })();
@@ -62,71 +131,17 @@ var mainController = (function (dataCtrl, UICtrl, mapCtrl) {
     */
     return {
 
+        init: function (google) {
+
+            // init google map and position
+            mapCtrl.init(google);
+
+        }
     }
 
-})();
+})(dataController, UIController, MapController);
 
-
-var MapConstants = {
-    DEFAULT_LATITUDE: 50.084218,
-    DEFAULT_LONGITUDE: 14.441196,
-    DEFAULT_ZOOM: 15,
-    DEFAULT_MAP_TYPE: 'terrain',
-}
-
-var currentLatitude = MapConstants.DEFAULT_LATITUDE;
-var currentLongitude = MapConstants.DEFAULT_LONGITUDE;
-var map = null;
 
 var initApp = function () {
-    findPosition();
-
-    var mapElement = document.getElementById('map');
-    var mapOptions = {
-        zoom: MapConstants.DEFAULT_ZOOM,
-        mapTypeId: MapConstants.DEFAULT_MAP_TYPE
-    };
-    map = new google.maps.Map(mapElement, mapOptions);
-    centerMap();
-};
-
-
-/*
-    center map
-  */
-var centerMap = function () {
-    map.setCenter(new google.maps.LatLng(parseFloat(currentLatitude), parseFloat(currentLongitude)));
-};
-
-/*
-find position - cheated code sttructure
-*/
-var findPosition = function () {
-    // get current location
-    // check if  Geolocation API is available
-    if (!navigator.geolocation) {
-        console.log('Geolocation API not available');
-        // Geolocation API not available - use default
-        currentLatitude = MapConstants.DEFAULT_LATITUDE;
-        currentLongitude = MapConstants.DEFAULT_LONGITUDE;
-
-    } else {
-        console.log('Geolocation API is available');
-        // asynchronous
-        navigator.geolocation.getCurrentPosition(function (position) {
-                // Get the coordinates of the current possition.
-                currentLatitude = position.coords.latitude;
-                currentLongitude = position.coords.longitude;
-                centerMap();
-            },
-            // Optional error callback
-            function (error) {
-                //alert('Position is unknown. It will be used a default one! Error code: ' + error.code)
-                console.log(error);
-                currentLatitude = MapConstants.DEFAULT_LATITUDE;
-                currentLongitude = MapConstants.DEFAULT_LONGITUDE;
-                centerMap();
-            }
-        );
-    }
+    mainController.init(google);
 };
